afd035868ee05570ecac729e765b11986518a202
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-10-09 23:36:32 +0530

    Update buildPipeline.groovy

--- buildPipeline.groovy
+++ buildPipeline.groovy
@@ -1,13 +1,22 @@
-node {
-   def mvn = tool name: 'MavenLatest', type: 'maven'
-  stage('SCM') {
-    checkout scm
-  }
-  stage('SonarQube Analysis') {
-   
-    	withSonarQubeEnv(credentialsId: 'sonarqubetoken') {
-      // Replace 'SonarQubeServer' with the ID of your configured SonarQube server
-      sh "${mvn}/bin/mvn clean install sonar:sonar -Dsonar.projectKey=SonarQubeScanner"
+pipeline {
+  agent any
+
+  stages {
+    stage('SCM') {
+      steps {
+        checkout scm
+      }
+    }
+
+    stage('SonarQube Analysis') {
+      steps {
+        script {
+          def mvnHome = tool name: 'MavenLatest', type: 'maven'
+          withSonarQubeEnv(credentialsId: 'sonarqubetoken') {
+            sh "${mvnHome}/bin/mvn clean install sonar:sonar -Dsonar.projectKey=SonarQubeScanner"
+          }
+        }
+      }
     }
   }
 }
