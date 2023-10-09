62e21f0439a47528b43a323263ea6f10499e8a7e
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-10-09 22:52:20 +0530

    Update buildPipeline.groovy

--- buildPipeline.groovy
+++ buildPipeline.groovy
@@ -1,43 +1,25 @@
-pipeline {
-    agent any
-
-    environment {
-        SONAR_SCANNER_HOME = tool name: 'Sonarqubescanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-        MAVEN_HOME = tool name: 'MavenLatest', type: 'maven'
-    }
-
-    stages {
-        stage('Code Checkout') {
-            steps {
-                echo 'Checking out code'
-                git changelog: false, credentialsId: 'GitHubCreds', poll: false, url: 'https://github.com/SiyaaJhawar/SonarQubeCoverageJava'
-            }
-        }
-
-        stage('Maven Build') {
-            steps {
-                echo 'Running Maven Build'
-                script {
-                    sh """
-                        ls -lart
-                        date
-                        ${MAVEN_HOME}/bin/mvn clean install
-                    """
-                }
-            }
-        }
-
-        stage('SonarQube Analysis') {
-            steps {
-                echo 'Running SonarQube Analysis'
-                script {
-                    withSonarQubeEnv(credentialsId: 'sonarqubetoken') {
-                        sh "${SONAR_SCANNER_HOME}/bin/sonar-scanner"
-                    }
-                }
-            }
-        }
-    }
+node(){
+    def sonarScanner = tool name: 'Sonarqubescanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+	stage('Code Checkout'){
+		git changelog: false, credentialsId: 'GitHubCreds', poll: false, url: 'https://github.com/SiyaaJhawar/MavenBuild'
+	}
+	stage('Maven Build'){
+		sh """
+			ls -lart
+			date
+			mvn clean install
+			
+		"""
+	}
+	stage('SonarQube Analysis') {
+    def mvn = tool name: 'MavenLatest', type: 'maven'
+  withSonarQubeEnv(credentialsId: 'SonarQubeToken') {
+			sh "${sonarScanner}/bin/sonar-scanner"
+      sh "${mvn}/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=SonarQubeScanner"
 }
-
-
+	    
+	}
+	stage('Code Deployment'){
+	
+	}
+}
