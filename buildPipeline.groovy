f209394a2ca7ea66827b35fe3e8ac58dca9dca91
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-10-09 22:43:55 +0530

    Update buildPipeline.groovy

--- buildPipeline.groovy
+++ buildPipeline.groovy
@@ -1,26 +1,43 @@
-node(){
-    def sonarScanner = tool name: 'Sonarqubescanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-    def MAVEN_HOME = tool name: 'MavenLatest', type: 'maven'
-	stage('Code Checkout'){
-		git changelog: false, credentialsId: 'GitHubCreds', poll: false, url: 'https://github.com/SiyaaJhawar/MavenBuild'
-	}
-	stage('Maven Build'){
-		sh """
-			ls -lart
-			date
-		 ${MAVEN_HOME}/bin/mvn clean install
-			
-		"""
-	}
-	stage('SonarQube Analysis'){
-	withSonarQubeEnv(credentialsId: 'sonarqubetoken') {
-    sh "ls -lart ${sonarScanner}/bin/sonar-scanner"
-}
-	    
-	}
-	stage('Code Deployment'){
-	
-	}
+pipeline {
+    agent any
+
+    environment {
+        SONAR_SCANNER_HOME = tool name: 'Sonarqubescanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+        MAVEN_HOME = tool name: 'MavenLatest', type: 'maven'
+    }
+
+    stages {
+        stage('Code Checkout') {
+            steps {
+                echo 'Checking out code'
+                git changelog: false, credentialsId: 'GitHubCreds', poll: false, url: 'https://github.com/SiyaaJhawar/SonarQubeCoverageJava'
+            }
+        }
+
+        stage('Maven Build') {
+            steps {
+                echo 'Running Maven Build'
+                script {
+                    sh """
+                        ls -lart
+                        date
+                        ${MAVEN_HOME}/bin/mvn clean install
+                    """
+                }
+            }
+        }
+
+        stage('SonarQube Analysis') {
+            steps {
+                echo 'Running SonarQube Analysis'
+                script {
+                    withSonarQubeEnv(credentialsId: 'sonarqubetoken') {
+                        sh "${SONAR_SCANNER_HOME}/bin/sonar-scanner"
+                    }
+                }
+            }
+        }
+    }
 }
 
 
