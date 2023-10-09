75de6a0569b25c8c817fa87667306293e177c21c
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-10-09 20:40:56 +0530

    Update buildPipeline.groovy

--- buildPipeline.groovy
+++ buildPipeline.groovy
@@ -1,23 +1,46 @@
-node(){
-    def sonarScanner = tool name: 'Sonarqubescanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-	def repoName = "https://github.com/SiyaaJhawar/SonarQubeCoverageJava"
-	stage('Code Checkout'){
-		git changelog: false, credentialsId: 'GitHubCreds', poll: false, url: repoName
-	}
-	stage('Maven Build'){
-		sh """
-			ls -lart
-			date
-			mvn clean install
-		"""
-	}
-		stage('code review'){
-	withSonarQubeEnv(credentialsId: 'sonarqubetoken') {
-    sh "ls -lart ${sonarScanner}/bin/sonar-scanner"
-}
-	    
-	}
-	stage('Code Deployment'){
-	
-	}
+pipeline {
+    agent any
+
+    environment {
+        SONAR_SCANNER_HOME = tool name: 'Sonarqubescanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+        MAVEN_HOME = tool name: 'MavenLatest', type: 'maven'
+    }
+
+    stages {
+        stage('Code Checkout') {
+            steps {
+                git changelog: false, credentialsId: 'GitHubCreds', poll: false, url: 'https://github.com/SiyaaJhawar/SonarQubeCoverageJava'
+            }
+        }
+
+        stage('Maven Build') {
+            steps {
+                script {
+                    echo 'Running Maven Build'
+                    sh """
+                        ls -lart
+                        date
+                        ${MAVEN_HOME}/bin/mvn clean install
+                    """
+                }
+            }
+        }
+
+        stage('SonarQube Analysis') {
+            steps {
+                script {
+                    echo 'Running SonarQube Analysis'
+                    withSonarQubeEnv(credentialsId: 'sonarqubetoken') {
+                        sh "${SONAR_SCANNER_HOME}/bin/sonar-scanner"
+                    }
+                }
+            }
+        }
+
+        stage('Code Deployment') {
+            steps {
+                // Add deployment steps as needed
+            }
+        }
+    }
 }
