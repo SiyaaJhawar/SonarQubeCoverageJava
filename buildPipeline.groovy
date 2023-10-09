9415462cd7ca29a04c2a324c720555c1dcb99aba
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-10-09 23:23:11 +0530

    Update buildPipeline.groovy

--- buildPipeline.groovy
+++ buildPipeline.groovy
@@ -1,23 +1,12 @@
-node(){
-    def sonarScanner = tool name: 'Sonarqubescanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-	stage('Code Checkout'){
-		git changelog: false, credentialsId: 'GitHubCreds', poll: false, url: 'https://github.com/SiyaaJhawar/MavenBuild'
-	}
-	stage('Maven Build'){
-		sh """
-			ls -lart
-			date
-			mvn clean install
-			
-		"""
-	}
-	stage('code review'){
-	withSonarQubeEnv(credentialsId: 'sonarqubetoken') {
-    sh "ls -lart ${sonarScanner}/bin/sonar-scanner"
-}
-	    
-	}
-	stage('Code Deployment'){
-	
-	}
+node {
+  stage('SCM') {
+    checkout scm
+  }
+  stage('SonarQube Analysis') {
+    def mvn = tool name: 'MavenLatest', type: 'maven'
+    withSonarQubeEnv(redentialsId: 'Sonarqubetoken') {
+      // Replace 'SonarQubeServer' with the ID of your configured SonarQube server
+      sh "${mvn}/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=SonarQubeScanner"
+    }
+  }
 }
