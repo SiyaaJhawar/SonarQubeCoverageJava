508089522040e9d9e9eb0bb58cd53cc04f66b574
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-10-10 16:12:15 +0530

    Update buildPipeline.groovy

--- buildPipeline.groovy
+++ buildPipeline.groovy
@@ -17,7 +17,7 @@ node {
     }
 
     stage('SonarQube Analysis') {
-        withSonarQubeEnv(installationName: `Sonarqubescanner`) {
+        withSonarQubeEnv(installationName: 'Sonarqubescanner') {
             
 
             sh "mvn sonar:sonar"
