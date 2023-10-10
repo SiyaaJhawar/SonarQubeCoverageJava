5239bb80cf44ba201c695e03f2c825d44c46a597
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-10-10 18:25:36 +0530

    Update buildPipeline.groovy

--- buildPipeline.groovy
+++ buildPipeline.groovy
@@ -1,7 +1,7 @@
 
 node {
     def sonarScanner = tool name: 'Sonarqubescanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-    def mvn = tool 'MavenLatest';
+  
     stage('Code Checkout') {
         checkout scm
         git credentialsId: 'GitHubCreds', poll: false, url: 'https://github.com/SiyaaJhawar/MavenBuild'
